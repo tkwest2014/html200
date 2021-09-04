177800e581f63e2177054b68c7b2aa91a54750ac
tkwest2014 <thomaswest@thomass-air.lan>
2021-09-04 12:06:55 -0500

    lesson2-challenge1

---
+++ lesson02/calculator.js
@@ -0,0 +1,9 @@
+const x = 9;
+const y = 2;
+const z = 'Hello World!';
+const t = 'My name is Kristen.';
+let age = (x*y-1)*2+1;
+
+console.log((x-y)*2);
+console.log(z+'  '+t);
+console.log('I am '+age+' years old.');
